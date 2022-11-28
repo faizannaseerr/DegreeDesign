b2f4b57ac34b934d2503f018dbedc3d119df017b
faizannaseer <f.naseer@mail.utoronto.ca>
2022-11-28 05:47:17 -0500

    You can no longer add invalid course codes

--- app/src/main/java/com/example/coursemanager/SecondFragmentAdmin.java
+++ app/src/main/java/com/example/coursemanager/SecondFragmentAdmin.java
@@ -165,9 +165,15 @@ public class SecondFragmentAdmin extends Fragment {
                         }
                         else{
 
+                            String courseCode = course.getCourseCode();
                             // Block course creation if no sections are selected
                             if (!course.isFall() && !course.isWinter() && !course.isSummer()){
                                 String warningMsg = "Please select offering sessions";
+                                Toast.makeText(getActivity(), warningMsg, Toast.LENGTH_LONG).show();
+                            }
+                            // Block course creation if course code is invalid
+                            else if (courseCode.length() != 6 || !courseCode.substring(0,3).matches("[A-Z]+") || !courseCode.substring(3,4).matches("^[A-D]") || !courseCode.substring(4).matches("^[0-9]*$")){
+                                String warningMsg = "Please enter a valid course code";
                                 Toast.makeText(getActivity(), warningMsg, Toast.LENGTH_LONG).show();
                             }
                             else{
