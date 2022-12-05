a9892ed90da9b5e71e82aaea3e1d0750e59c9039
Eddy <eddy.al.chen@gmail.com>
2022-12-05 01:40:48 -0500

    Timeline Generation (ThirdFragmentStudent) now working

--- app/src/main/java/com/example/coursemanager/ThirdFragmentStudent.java
+++ app/src/main/java/com/example/coursemanager/ThirdFragmentStudent.java
@@ -1,5 +1,6 @@
 package com.example.coursemanager;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.example.coursemanager.databinding.FragmentFirstStudentBinding;
@@ -66,7 +68,8 @@ public class ThirdFragmentStudent extends Fragment {
         });
 
         ArrayList<Course> CoursesWanted = new ArrayList<Course>();
-        ArrayList<Course> CoursesTaken = new ArrayList<Course>();
+        ArrayList<String> CoursesTaken = new ArrayList<String>();
+        ArrayList<Course> CoursesScheduled = new ArrayList<Course>();
 
         DatabaseReference dReference = FirebaseDatabase
                 .getInstance("https://course-manager-b07-default-rtdb.firebaseio.com/")
@@ -78,9 +81,7 @@ public class ThirdFragmentStudent extends Fragment {
                         child(((MainActivityStudent) getActivity()).getUsername())
                         .child("coursesTaken");
                 for (DataSnapshot snapshot: snap.getChildren()){
-                    Course TakenCourse = new Course();
-                    TakenCourse.setCourseCode(snapshot.getKey());
-                    CoursesTaken.add(TakenCourse);
+                    CoursesTaken.add(snapshot.getKey());
                     // Don't need to add more details for Courses Taken I believe, other than course codes
                 }
 
@@ -111,21 +112,144 @@ public class ThirdFragmentStudent extends Fragment {
 
                 // Keep ur code to display inside this block ***
 
-
-                ArrayList<String> print = new ArrayList<>();
-                for (int i = 0; i < CoursesWanted.size(); i++){
-                    print.add(CoursesWanted.get(i).getCourseCode());
+                // creating list of things to schedule
+                while (!CoursesWanted.isEmpty()) {
+                    Course a = CoursesWanted.get(0);
+                    CoursesScheduled.add(a);
+                    for (String b: a.getPrereqs()){
+                        Course c = dataSnapshot.child("Courses").child(b).getValue(Course.class);
+                        boolean found = false;
+                        for (String d: CoursesTaken){
+                            if (d.compareTo(b) == 0){
+                                found = true;
+                            }
+                        }
+                        for (Course d: CoursesWanted){
+                            if (d.getCourseCode().compareTo(b) == 0){
+                                found = true;
+                            }
+                        }
+                        for (Course d: CoursesScheduled){
+                            if (d.getCourseCode().compareTo(b) == 0){
+                                found = true;
+                            }
+                        }
+                        if (!found){
+                            CoursesWanted.add(c);
+                        }
+                    }
+                    CoursesWanted.remove(0);
                 }
-                Log.d("Debug",CoursesWanted.toString());
-                Log.d("Debug",CoursesTaken.toString());
+
+                // creating schedule
+                int i = 0;
+                int j = 0;
+                boolean morethanfive = false;
+                ArrayList<Course> presentSemester = new ArrayList<Course>();
+                while(!CoursesScheduled.isEmpty()) {
+                    for (Course a: CoursesScheduled){
+                        boolean takeable = true;
+                        if (j == 0 && !a.isFall()){
+                            takeable = false;
+                        }
+                        if (j == 1 && !a.isWinter()){
+                            takeable = false;
+                        }
+                        if (j == 2 && !a.isSummer()){
+                            takeable = false;
+                        }
+                        for (String b: a.getPrereqs()){
+                            boolean pretaken = false;
+                            for (String c: CoursesTaken){
+                                if (b.compareTo(c) == 0){
+                                    pretaken = true;
+                                }
+                            }
+                            if (!pretaken) {
+                                takeable = false;
+                            }
+                        }
+                        if (takeable) {
+                            presentSemester.add(a);
+                        }
+                    }
+
+                    //adding row to table
+                    int k = 0;
+                    TableRow row = new TableRow(getActivity());
+                    TableRow.LayoutParams params =
+                            new TableRow.LayoutParams(TableRow
+                                    .LayoutParams.WRAP_CONTENT);
+                    row.setLayoutParams(params);
+                    row.setId(1000 + (i * 3 + j) * 1000);
+                    TextView semester = new TextView(getActivity());
+                    semester.setId(1001 + (i * 3 + j) * 1000);
+                    semester.setText("");
+                    int year = i + 2022;
+                    if (j == 0){
+                        semester.setText(year + " Fall: ");
+                    }
+                    if (j == 1){
+                        year ++;
+                        semester.setText(year + " Winter: ");
+                    }
+                    if (j == 2){
+                        year ++;
+                        semester.setText(year + " Summer: ");
+                    }
+                    row.addView(semester);
+                    TextView courseList = new TextView(getActivity());
+                    String longName = "";
+                    while (!presentSemester.isEmpty()){
+                        Course a = presentSemester.get(0);
+                        if (k != 0){
+                            longName = longName + ", ";
+                        }
+                        longName = longName + a.getCourseCode();
+                        for (Course b: CoursesScheduled){
+                            if (b.getCourseCode().compareTo(a.getCourseCode()) == 0){
+                                CoursesScheduled.remove(b);
+                                break;
+                            }
+                        }
+                        CoursesTaken.add(a.getCourseCode());
+                        presentSemester.remove(0);
+                        k ++;
+                    }
+                    if (k > 4){
+                        morethanfive = true;
+                    }
+                    courseList.setText(longName);
+                    courseList.setId(1002 + (i * 3 + j) * 1000);
+                    row.addView(courseList);
+                    binding.table.addView(row, i * 3 + j);
+                    j ++;
+                    if (j == 3){
+                        i ++;
+                        j = 0;
+                    }
+                }
+
+                if (morethanfive){
+                    binding.textView.setText("Please consult with your program coordinator");
+                    binding.textView2.setText("before taking more than 5 courses in a semester");
+                }
+
+
+                //ArrayList<String> print = new ArrayList<>();
+                //for (int i = 0; i < CoursesWanted.size(); i++){
+                    //print.add(CoursesWanted.get(i).getCourseCode());
+                //}
+                //Log.d("Debug",CoursesWanted.toString());
+                //Log.d("Debug",CoursesTaken.toString());
 
                 /* Courses Taken isn't showing up on the screen ugh */
 
-                Schedule useless = new Schedule ();
-                ArrayList<Course> TotalCourses = new ArrayList<Course>();
+                //Schedule useless = new Schedule ();
+                //ArrayList<Course> TotalCourses = new ArrayList<Course>();
 
-                TotalCourses = useless.CreateTotalCoursesArray(CoursesTaken, CoursesWanted, TotalCourses);
-                ArrayList<Schedule> DegreeSchedule = useless.CreateSchedule(CoursesTaken, TotalCourses);
+                //TotalCourses = useless.CreateTotalCoursesArray(CoursesTaken, CoursesWanted, TotalCourses);
+                //ArrayList<Schedule> DegreeSchedule = useless.CreateSchedule(CoursesTaken, TotalCourses);
 
                 /* Last: with the schedule list use year and semester field to display courses -
                      degree year 1 means 2022 and so on
@@ -135,8 +259,8 @@ public class ThirdFragmentStudent extends Fragment {
                  */
 
 
-                ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, print);
-                binding.listview.setAdapter(adapter);
+                //ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, print);
+                //binding.listview.setAdapter(adapter);
 
                 // Currently using this to test schedule algorithm & generation of Courses Wanted & Taken Arrays ^^
 
